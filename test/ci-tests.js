e46425a888df0f63857777586586bf2d10493b6c
Kayla Webb <kaylarwebb@gmail.com>
2017-12-04 20:52:00 -0500

    Created test to check for text/html file.

--- test/ci-tests.js
+++ test/ci-tests.js
@@ -4,9 +4,14 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const mocha = require('mocha');
 const should = chai.should();
+const chaiFiles = require('chai-files');
+const expect = chai.expect;
+const file = chaiFiles.file;
+const dir = chaiFiles.dir;
 const { app, runServer, closeServer } = require('../server');
 const { TEST_DATABASE_URL } = require('../config');
 
+chai.use(chaiFiles);
 chai.use(chaiHttp);
 
 // const tearDownDb = () => {
@@ -27,13 +32,14 @@ describe('Food Item API Resource', function() {
   });
   
   describe('GET root endpoint', function() {
-    it('should return static HTML', function() {
+    it('should serve a static HTML file', function() {
       return chai.request(app)
         .get('/')
         .then(function(res) {
+          console.log(res.headers);
           res.should.have.status(200);
+          res.should.have.header('content-type', 'text/html; charset=UTF-8');
         });
-  
 
     });
 
