aaeae01fe93bffec2c355679980df06703e41bc8
Firoz Kamdar <firozkamdar@gmail.com>
2017-12-05 08:02:29 -0800

    mock client data added, build started

---
+++ public/app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const MOCK_NUTRITION_UPDATES = {
+    "foodItemUpdates": [
+        {
+            "Name": "Wild Rice",
+            "Serving Size": 1,
+            "Fat": 10,
+            "Carbs": 30,
+            "Protein": 25,
+            "Total Calories": 300
+        },
+        {
+            "Name": "Apple",
+            "Serving Size": 1,
+            "Fat": 10,
+            "Carbs": 25,
+            "Protein": 15,
+            "Total Calories": 200
+        },
+        {
+            "Name": "Potato",
+            "Serving Size": 1,
+            "Fat": 10,
+            "Carbs": 25,
+            "Protein": 15,
+            "Total Calories": 200
+        },
+        {
+            "Name": "Turkey",
+            "Serving Size": 1,
+            "Fat": 10,
+            "Carbs": 25,
+            "Protein": 15,
+            "Total Calories": 200
+        }
+    ]
+};
